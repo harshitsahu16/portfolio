09f3221e779af894f726a950927e08023c2ee54a
Harshit <harshitsahu160@gmail.com>
2023-06-28 14:36:42 +0530

    navbar updated

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -82,7 +82,7 @@ const Navbar = () => {
             <div>
                 <div className='flex w-full items-center justify-between'>
                     <Link href='/' >
-                    <Image src='/../public/assets/navLogo.png' width='87' height='35'  alt='/' />
+                    <Image src={Logo} width='87' height='35'  alt='/' />
                     </Link>
                     <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
                         <AiOutlineClose />
